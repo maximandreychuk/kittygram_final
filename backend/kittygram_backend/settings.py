c718af9005a2eaf5478842b5b41c84c884e09f43
7ras0tresh <maximandreychuk1999@gmail.com>
2023-06-11 21:10:39 +0300

    add default value in sett

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = bool(os.getenv('DEBUG', True))
 
-ALLOWED_HOSTS = [os.getenv('DOMAIN'), 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('DOMAIN', 'domain'), 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
