d35534cd54f754080cd276432318a47bee5816fb
7ras0tresh <maximandreychuk1999@gmail.com>
2023-06-11 21:47:32 +0300

    ovrwrt sett

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$')
 
-DEBUG = bool(os.getenv('DEBUG', True))
+DEBUG = bool(os.getenv('DEBUG', False))
 
-ALLOWED_HOSTS = [os.getenv('DOMAIN', 'domain'), 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('DOMAIN', 'kittyk.ddns.net'), 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
